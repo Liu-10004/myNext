6399f83c912e0f280dbd7ec4e4de087f8d6e20c3
liubingqun <814846013@qq.com>
2020-06-04 19:11:26 +0800

    第一次提交

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const withLess = require('@zeit/next-less');
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    importLoaders: 1,
+    localIdentName: '[local]___[hash:base64:5]'
+  },
+  distDir: 'build'
+});
